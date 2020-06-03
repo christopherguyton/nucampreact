f15904f52f25deac204e5505b96be3cac4f8ab0e
MrBehavior <cguyton87@gmail.com>
2020-06-02 21:04:59 -0700

    new nucamp files

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,55 +1,65 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import {
+  Card,
+  CardImg,
+  //   CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle,
+} from 'reactstrap';
 
 class CampsiteInfo extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {}
-    };
-    
-renderCampsite(campsite) {
-    return(
+  renderCampsite(campsite) {
+    if (campsite) {
+      return (
         <div className="col-md-5 m-1">
-        <Card>
-        <CardImg top src={campsite.image} alt={campsite.name} />
-        <CardBody>
-            <CardTitle>{campsite.name}</CardTitle>
-            <CardText>{campsite.description}</CardText>
-        </CardBody>
-         </Card>
+          <Card>
+            <CardImg top src={campsite.image} alt={campsite.name} />
+            <CardBody>
+              <CardTitle>{campsite.name}</CardTitle>
+              <CardText>{campsite.description}</CardText>
+            </CardBody>
+          </Card>
         </div>
-    )
-}
+      );
+    }
+    return <div />;
+  }
 
-renderComments(comments) {
+  renderComments(comments) {
     if (comments) {
-        return (
-            <div className = "col-md-5 m-1">
-            <h4>Comments</h4>
-            {comments.map(comment => { return (<div key={comment.id}><p>{comment.text}<br/>-- {comment.author},
-                <br/>{new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p></div> ); })}
-            
+      return (
+        <div className="col-md-5 m-1">
+          <h4>Comments</h4>
+          {comments.map((comment) => (
+            <div key={comment.id}>
+              <p>{comment.text}</p>
+              <p>
+                -- {comment.author},{' '}
+                {new Intl.DateTimeFormat('en-US', {
+                  year: 'numeric',
+                  month: 'short',
+                  day: '2-digit',
+                }).format(new Date(Date.parse(comment.date)))}
+              </p>
             </div>
-        );
+          ))}
+        </div>
+      );
     }
-}
-
-    render() {
+  }
+render() {
         if (this.props.campsite) {
-            return(
+            return (
                 <div className="container">
-                <div className="row">
-                {this.renderCampsite(this.props.campsite)} 
-                {this.renderComments(this.props.campsite.comments)}
+                    <div className="row">
+                        {this.renderCampsite(this.props.campsite)}
+                        {this.renderComments(this.props.campsite.comments)}
+                    </div>
                 </div>
-                </div>
-            )
-        } else { 
-             return(
-            <div />
-        )
+            );
         }
-    }
+        return <div />;
 }
-
+}
 export default CampsiteInfo;
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import Directory from './DirectoryComponent';
-import CampsiteInfo from './CampsiteInfoComponent';
-import { CAMPSITES } from '../shared/campsites';
+import React, { Component } from "react";
+import { Navbar, NavbarBrand } from "reactstrap";
+import Directory from "./DirectoryComponent";
+import CampsiteInfo from "./CampsiteInfoComponent";
+import { CAMPSITES } from "../shared/campsites";
 
 class Main extends Component {
     constructor(props) {
         super(props);
         this.state = {
             campsites: CAMPSITES,
-            selectedCampsite: null
+            selectedCampsite: null,
         };
     }
 
     onCampsiteSelect(campsiteId) {
-        this.setState({selectedCampsite: campsiteId});
+        this.setState({ selectedCampsite: campsiteId });
     }
 
     render() {
@@ -25,11 +25,20 @@ class Main extends Component {
                         <NavbarBrand href="/">NuCamp</NavbarBrand>
                     </div>
                 </Navbar>
-                <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
-                <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
+                <Directory
+                    campsites={this.state.campsites}
+                    onClick={(campsiteId) => this.onCampsiteSelect(campsiteId)}
+                />
+                <CampsiteInfo
+                    campsite={
+                        this.state.campsites.filter(
+                            (campsite) => campsite.id === this.state.selectedCampsite
+                        )[0]
+                    }
+                />
             </div>
         );
-    };
+    }
 }
 
 export default Main;
