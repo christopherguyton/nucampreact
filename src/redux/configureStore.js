f4cd3f6ece7320e1f72967f013da87f4f141bcdf
MrBehavior <cguyton87@gmail.com>
2020-06-09 18:55:31 -0700

    added redux

---
+++ src/redux/configureStore.js
@@ -0,0 +1,7 @@
+import {createStore} from 'redux';
+import {Reducer, intialState, initialState} from './reducer';
+
+export const ConfigureStore = () => {
+    const store = createStore(Reducer, initialState);
+    return store;
+};
